4ee9c565273701e3bff7d60c47145514c13d32f5
michelle-b7 <bai7michelle@gmail.com>
2024-10-07 23:57:51 -0700

    Added Game stuff

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -2,136 +2,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    /*void Start()
+    [SerializeField] float boostModifier;
+    public Rigidbody2D rb;
+    public Vector2 input_dir;
+
+
+    private void Start()
     {
-        bool isWounded = true;
-        bool isBruised = false;
-
-
-        if (isWounded && isBruised)
-        {
-            Debug.Log("dead");
-
-        }
-        else if (isWounded && !isBruised)
-        {
-            Debug.Log("ouch");
-        }
-
-
-        string animal = "Human";
-        if (animal == "Human")
-        {
-            Debug.Log("teehee");
-        }
-
-
-        for (int i = 1; i <= 5; i++)
-        {
-            //prints loop # 1 through 5
-            Debug.Log("loop" + i);
-
-
-        }
-
-    }*/
-
-    void Start()
-    {
-
+        
+        rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // jetpack
+        // rb.AddForce(input_dir * boostModifier);
+    }
+    private void OnMove(InputValue value)
+    {
+        Vector2 movement_dir = value.Get<Vector2>();
+        
+        // restrain vertical movement
+        movement_dir = new Vector2(movement_dir.x, 0).normalized;
+        Debug.Log(movement_dir);
+        rb.AddForce(movement_dir * boostModifier, ForceMode2D.Impulse);
+        // jetpack
+        // input_dir = movement_dir;
+        
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("boing");
     }
 
 
-    void LetterGrade(float grade)
-    {
-        if (grade >= 0.9)
-        {
-            Debug.Log("A");
-        }
-        else if (grade >= 0.8)
-        {
-            Debug.Log("B");
-        }
-        else if(grade >= 0.7)
-        {
-            Debug.Log("C");
-        }
-        else if (grade >= 0.6)
-        {
-            Debug.Log("D");
-        }
-        else
-        {
-            Debug.Log("F");
-        }
-
-    }
-
-    /*List<int> CreateList()
-    {
-
-        // foreach loops
-        // creating list storing int values
-        List<int> numbers = new List<int>();
-
-        // filling list
-        numbers.Add(10);
-        numbers.Add(20);
-        numbers.Add(30);
-        numbers.Add(5);
-
-        // remove element
-        numbers.Remove(20);
-
-        // sort list in ascending order
-        numbers.Sort();
-
-        // compute total sum and then add that to the list
-        int sum = 0;
-        foreach (int number in numbers)
-        {
-            sum += number;
-        }
-        numbers.Add(sum);
-        // return list
-        return numbers;
-
-    }*/
 
 }
 
-
-
-// classes
-
-/*public class House
-{
-    public string name = "my house";
-
-    public bool Renovate()
-    {
-        return true;
-    }
-
-    House myHouse = new House();
-
-    bool success = myHouse.Renovate();
-
-    string house_name = myHouse.name;
-}*/
-
-// WHY IS THIS NOT WORKING BREHHHH
 
 
 
